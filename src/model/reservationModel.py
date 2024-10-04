8b627c15f376c5f12282836d0153355bf0a5d9db
Alonso <alonsonh94@gmail.com>
2024-10-04 16:25:26 -0600

    Update model

--- src/model/reservationModel.py
+++ src/model/reservationModel.py
@@ -4,12 +4,10 @@ from pydantic import BaseModel
 class ReservationModel(BaseModel):
     reservation_id: int = 123
     name: str = ""
+    phoneNumber: str = ""
     email: str = ""
+    insurance: str = ""
     message: str = ""
+    date: str = ""
+    hour: str = ""
 
-    def __init__(self, reservation_id: int, name: str, email: str, message: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.reservation_id = reservation_id
-        self.name = name
-        self.email = email
-        self.message = message
