c2ddc8c65ed40ebcce9350292c3c2b7a60909ae5
Alonso VN <alonsonh94@gmail.com>
2025-03-08 14:27:22 -0600

    Ref: rename

--- src/api/testimonialApi.py
+++ src/api/testimonialApi.py
@@ -7,97 +7,97 @@ from src.utils.logUtil import log
 
 router = APIRouter()
 
-mock_testimonies = [
+mock_testimonials = [
     {
         "id": 1,
         "firstName": 'Elena',
         "lastName": 'Ramirez',
         "rating": 4,
-        "testimony": 'El personal fue muy atento y la clínica es excelente. Mi rodilla se siente mucho mejor.',
+        "comment": 'El personal fue muy atento y la clínica es excelente. Mi rodilla se siente mucho mejor.',
     },
     {
         "id": 2,
         "firstName": 'Carlos',
         "lastName": 'Gomez',
         "rating": 5,
-        "testimony": 'El servicio superó mis expectativas. El Dr. [Apellido del Doctor] es muy conocedor y atento. ¡Muy recomendado!',
+        "comment": 'El servicio superó mis expectativas. El Dr. [Apellido del Doctor] es muy conocedor y atento. ¡Muy recomendado!',
     },
     {
         "id": 3,
         "firstName": 'Sofia',
         "lastName": 'Lopez',
         "rating": 3,
-        "testimony": 'Aceptable, buen servicio en general. Mis sesiones de fisioterapia han sido útiles.',
+        "comment": 'Aceptable, buen servicio en general. Mis sesiones de fisioterapia han sido útiles.',
     },
     {
         "id": 4,
         "firstName": 'Javier',
         "lastName": 'Hernandez',
         "rating": 5,
-        "testimony": 'Excelente calidad de atención y dedicación. Estoy muy satisfecho con mi recuperación.',
+        "comment": 'Excelente calidad de atención y dedicación. Estoy muy satisfecho con mi recuperación.',
     },
     {
         "id": 5,
         "firstName": 'Isabela',
         "lastName": 'Martinez',
         "rating": 4,
-        "testimony": 'La clínica es muy limpia y los doctores son muy profesionales. Mi dolor de hombro ha disminuido significativamente.',
+        "comment": 'La clínica es muy limpia y los doctores son muy profesionales. Mi dolor de hombro ha disminuido significativamente.',
     },
     {
         "id": 6,
         "firstName": 'Valentina',
         "lastName": 'Sanchez',
         "rating": 5,
-        "testimony": 'Todo fue perfecto, desde la recepción hasta el tratamiento. Me sentí muy cómoda.',
+        "comment": 'Todo fue perfecto, desde la recepción hasta el tratamiento. Me sentí muy cómoda.',
     },
     {
         "id": 7,
         "firstName": 'Mateo',
         "lastName": 'Diaz',
         "rating": 4,
-        "testimony": 'Buen lugar para atención ortopédica. Las instalaciones son modernas y el equipo es amigable.',
+        "comment": 'Buen lugar para atención ortopédica. Las instalaciones son modernas y el equipo es amigable.',
     },
     {
         "id": 8,
         "firstName": 'Camila',
         "lastName": 'Ruiz',
         "rating": 3,
-        "testimony": 'Buen servicio y opciones de tratamiento variadas. Mi espalda se siente mucho mejor.',
+        "comment": 'Buen servicio y opciones de tratamiento variadas. Mi espalda se siente mucho mejor.',
     },
     {
         "id": 9,
         "firstName": 'Sebastian',
         "lastName": 'Torres',
         "rating": 5,
-        "testimony": 'El mejor servicio ortopédico que he recibido. ¡Gracias!',
+        "comment": 'El mejor servicio ortopédico que he recibido. ¡Gracias!',
     },
     {
         "id": 10,
         "firstName": 'Lucia',
         "lastName": 'Vargas',
         "rating": 4,
-        "testimony": "Muy contenta con el servicio y la calidad de la atención. Mi tobillo está sanando bien."
+        "comment": "Muy contenta con el servicio y la calidad de la atención. Mi tobillo está sanando bien."
     }
 ]
 
 
-@router.get("/", response_model=List[TestimonialResponse])
+@router.get("/all", response_model=List[TestimonialResponse])
 async def get_all_testimonials():
     try:
-        return mock_testimonies
+        return mock_testimonials
 
     except Exception as e:
-        log.info(f"Error getting opinions. Exception: {e}")
+        log.info(f"Error getting testimonials. Exception: {e}")
 
 
 @router.post("/", response_model=TestimonialResponse, status_code=status.HTTP_201_CREATED)
-async def create_testimony(testimonial: TestimonialCreate):
+async def create_testimonial(testimonial: TestimonialCreate):
     try:
-        new_id = len(mock_testimonies) + 1
+        new_id = len(mock_testimonials) + 1
         new_testimonial = {"id": new_id, **testimonial.model_dump()}
-        mock_testimonies.append(new_testimonial)
+        mock_testimonials.append(new_testimonial)
 
         return new_testimonial
 
     except Exception as e:
-        log.info(f"Error getting opinions. Exception: {e}")
+        log.info(f"Error creating testimonial. Exception: {e}")
--- src/schema/testimonialSchema.py
+++ src/schema/testimonialSchema.py
@@ -5,7 +5,7 @@ class TestimonialCreate(BaseModel):
     firstName: str = Field(..., example="First name")
     lastName: str = Field(..., example="Last name")
     rating: int = Field(..., ge=1, le=5, example=5)
-    testimony: str = Field(..., example="Great service!")
+    comment: str = Field(..., example="Great service!")
 
 
 class TestimonialResponse(TestimonialCreate):
