a6a640627b7b6a75fd980dc446bbd5c589b1a713
Alonso VN <alonsonh94@gmail.com>
2025-03-18 23:30:13 -0600

    Enhance columns name and definition

--- src/models/user.py
+++ src/models/user.py
@@ -2,7 +2,7 @@ from src.database.postgres import Base
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import Column, String, Boolean, DateTime, func
 
 
 class UserModel(Base):
@@ -10,7 +10,9 @@ class UserModel(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
     name = Column(String(255))
-    email = Column(String(255), nullable=False)
+    email = Column(String(255),unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
-    isAdmin = Column(Boolean, nullable=False, default=False)
-    isActive = Column(Boolean, nullable=False, default=True)
+    is_admin = Column(Boolean, nullable=False, default=False)
+    is_active = Column(Boolean, nullable=False, default=True)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
