329d159fca5c726355eeb6e8deae500c2bc61f57
Alonso VN <alonsonh94@gmail.com>
2025-03-18 23:31:10 -0600

    Enhanced code, logging, name, error handling

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,18 +1,73 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
 from sqlalchemy.orm import Session
 
 from src.database.postgres import get_db
 from src.schemas.user import UserToken, UserCreate, UserLogin
-from src.services.user_service import register_user, login_user
+from src.services.auth_service import generate_access_token, verify_access_token
+from src.services.user_service import register_user, login_user, get_user_by_email
+from src.utils.logUtil import log
 
 router = APIRouter()
 
+# OAuth2PasswordBearer: This is used to get the token from Authorization header
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 
 @router.post("/register", response_model=UserToken)
-def register(user: UserCreate, db: Session = Depends(get_db)):
-    return register_user(user, db)
+async def register(user: UserCreate, db: Session = Depends(get_db)):
+    log.info(f"Attempting to register user with email: {user.email}")
+
+    registered_user = register_user(user, db)
+    if not registered_user:
+        log.error("User registration failed due to a database issue")
+        raise HTTPException(status_code=500, detail="Database error: Could not register the user")
+
+    user_access_token = generate_access_token(registered_user)
+    log.info(f"User {user.email} registered successfully")
+
+    return user_access_token
 
 
 @router.post("/login", response_model=UserToken)
-def login(user: UserLogin, db: Session = Depends(get_db)):
-    return login_user(user, db)
+async def login(user: UserLogin, db: Session = Depends(get_db)):
+    log.info(f"Attempting login for user: {user.email}")
+
+    logged_user = login_user(user, db)
+    if not logged_user:
+        log.warning(f"Login failed for user {user.email}: Invalid credentials")
+        raise HTTPException(status_code=401, detail="invalid credentials")
+
+    user_access_token = generate_access_token(logged_user)
+    log.info(f"User {user.email} logged in successfully")
+
+    return user_access_token
+
+
+@router.post("/refresh-token", response_model=UserToken)
+async def refresh_token(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    log.info("Attempting to refresh token")
+
+    try:
+        # Validate the token and extract user info
+        payload = verify_access_token(token)
+        user_email = payload.get("sub")
+
+        if not user_email:
+            log.warning("Refresh token is missing 'sub' field or is invalid.")
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+        user_db = get_user_by_email(user_email, db)
+        if not user_db:
+            log.warning(f"User with email {user_email} not found.")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+        new_access_token = generate_access_token(user_db)
+        log.info(f"Successfully refreshed token for user: {user_email}")
+
+        return new_access_token
+
+    except JWTError as e:
+        log.error(f"Error during token refresh: {str(e)}", exc_info=True)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token")
--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -5,11 +5,12 @@ from jose import JWTError, jwt
 from fastapi import HTTPException, status
 
 from src.core.app_config import AppConfig
+from src.models.user import UserModel
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-auth_config = AppConfig().config.get("authentication")
-secret_key = auth_config.get("secretKey")
+auth_config = AppConfig().config.get("authentication", {})
+secret_key = auth_config.get("secretKey", "")
 algorithm = auth_config.get("algorithm", "HS256")
 access_token_expires_in = auth_config.get("access_token_expire_minutes", 15)
 
@@ -22,19 +23,26 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=access_token_expires_in))
-    to_encode.update({"exp": expire})
+def generate_access_token(user: UserModel, expires_delta: Optional[timedelta] = None) -> str:
+    expires_delta = expires_delta or timedelta(minutes=access_token_expires_in)
+    expiration = datetime.now(timezone.utc) + expires_delta
 
-    return jwt.encode(to_encode, secret_key, algorithm=algorithm)
+    payload = {
+        "id": str(user.id),
+        "userName": user.name,
+        "sub": user.email,
+        "exp": expiration.timestamp()
+    }
+    access_token = jwt.encode(payload, secret_key, algorithm=algorithm)
+
+    return access_token
 
 
-def verify_access_token(token: str):
+def decode_token(token: str):
     try:
         payload = jwt.decode(token, secret_key, algorithms=[algorithm])
 
-        return payload  # Contains user data
+        return payload
 
     except JWTError:
         raise HTTPException(
@@ -42,3 +50,7 @@ def verify_access_token(token: str):
             detail="Invalid authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+
+def verify_access_token(token: str) -> dict:
+    return decode_token(token)
