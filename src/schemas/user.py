fb064bb11e4a6d4e0efd1c97fc7e12d8d64ff795
Alonso VN <alonsonh94@gmail.com>
2025-03-20 22:22:41 -0600

    Add RefreshTokenRequest

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,19 +1,23 @@
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
-class UserCreate(BaseModel):
+class UserCreateRequest(BaseModel):
     email: EmailStr
     password: str  # TODO validate password
 
 
-class UserLogin(BaseModel):
+class UserLoginRequest(BaseModel):
     email: EmailStr
     password: str
 
 
-class UserToken(BaseModel):
-    access_token: str
-    token_type: str
-    refresh_token: Optional[str] = None
+class UserTokenResponse(BaseModel):
+    access_token: str = Field(..., alias="accessToken")
+    token_type: str = Field(..., alias="tokenType")
+    refresh_token: Optional[str] = Field(..., alias="refreshToken")
+
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str = Field(..., alias="refreshToken")
