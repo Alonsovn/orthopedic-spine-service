9e410e90ddae549b4b13b0de920d6c943825c6dd
Alonso VN <alonsonh94@gmail.com>
2025-03-23 15:18:48 -0600

    Add EmailService class

--- src/routes/email_routes.py
+++ src/routes/email_routes.py
@@ -1,42 +1,28 @@
-import os
-import smtplib
 
-from fastapi import APIRouter, status, Depends
-from email.mime.text import MIMEText
+from fastapi import APIRouter, status, HTTPException
 
-from src.core.app_config import AppConfig
-from src.dependencies.auth_dependency import get_current_user
 from src.schemas.email import EmailSchema
+from src.services.emai_service import EmailService
 from src.utils.logUtil import log
 
 router = APIRouter()
 
-email_config: dict = AppConfig().config.get("email")
 
-# Email configuration
-SMTP_SERVER = email_config.get("smtpServer")
-SMTP_PORT = email_config.get("smtpPort")
-EMAIL_USERNAME = email_config.get("address")
-EMAIL_PASSWORD = email_config.get("password")
+@router.post("/receive-email", status_code=status.HTTP_201_CREATED)
+async def receive_email(email_payload: EmailSchema):
+    email_service = EmailService()
+    email_service.receive_email_from_client(email_payload)
+
+    return {"success": "Email received successfully"}
 
 
-@router.post("/send-email", status_code=status.HTTP_201_CREATED)
-async def send_email(payload: EmailSchema, current_user: str = Depends(get_current_user)):
-    try:
-        msg = MIMEText(payload.message)
-        msg["Subject"] = payload.subject
-        msg["From"] = payload.fromUser
-        msg["To"] = EMAIL_USERNAME
+@router.post("/send-verification-code-email", status_code=status.HTTP_200_OK)
+async def send_verification_code_email(email: str):
+    if not email:
+        log.error("Missing email parameter")
+        raise HTTPException(status_code=500, detail="Verification error: Could not send the verification code")
 
-        log.info(f"Email: {msg}")
+    email_service = EmailService()
+    verification_code = email_service.send_code_verification_email(email)
 
-        with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
-            server.login(EMAIL_USERNAME, EMAIL_PASSWORD)
-            server.send_message(msg)
-
-        log.info("Email sent successfully")
-
-        return {"success": "Email sent successfully"}
-
-    except Exception as e:
-        log.info(f"Error sending email. Exception: {e}")
+    return {"verificationCode": verification_code}
---
+++ src/services/emai_service.py
@@ -0,0 +1,73 @@
+import random
+
+from src.core.app_config import AppConfig
+from src.schemas.email import EmailSchema
+from email.mime.text import MIMEText
+import smtplib
+
+from src.utils.logUtil import log
+
+email_config: dict = AppConfig().config.get("email")
+
+
+class EmailService:
+
+    def __init__(self):
+        self.smtp_server = email_config.get("smtpServer", "smtp.gmail.com")
+        self.smtp_port = email_config.get("smtpPort", 465)
+        self.email_address = email_config.get("address", "orthopedicspineinfo@gmail.com")
+        self.email_password = email_config.get("password", "")
+
+    def send_email(self, email_content: MIMEText):
+        log.info(f"Sending email: {email_content}")
+
+        try:
+            with smtplib.SMTP_SSL(self.smtp_server, self.smtp_port) as server:
+                server.login(self.email_address, self.email_password)
+                server.send_message(email_content)
+            log.info("Email sent successfully")
+
+            return True
+
+        except Exception as e:
+            log.info(f"Error sending email. Exception: {e}")
+
+    def receive_email_from_client(self, email_payload: EmailSchema):
+
+        try:
+
+            email_message = MIMEText(email_payload.message)
+            email_message["Subject"] = f"Mensaje de: {email_payload.subject}"
+            email_message["From"] = email_payload.fromUser
+            email_message["To"] = self.email_address
+
+            self.send_email(email_message)
+
+            return {"success": "Email sent successfully"}
+
+        except Exception as e:
+            log.info(f"Error sending email. Exception: {e}")
+
+    def send_code_verification_email(self, email_to: str):
+        log.info(f"Sending verification code to: {email_to}")
+
+        try:
+
+            verification_code = self.generate_verification_code()
+            message = f"Este es tu número de verificación: {verification_code}"
+
+            email_message = MIMEText(message)
+            email_message["Subject"] = "Código de verificación"
+            email_message["From"] = self.email_address
+            email_message["To"] = email_to
+
+            log.info("Code verification email sent successfully")
+
+            return verification_code
+
+        except Exception as e:
+            log.info(f"Error sending verification code email. Exception: {str(e)}")
+
+    @staticmethod
+    def generate_verification_code():
+        return random.randint(1000, 9999)
