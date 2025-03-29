68fe48e7df6465ab8a58e928cefc26449b787f75
Alonso VN <alonsonh94@gmail.com>
2025-03-28 18:31:46 -0600

    Ref: create service class

--- src/services/testimonial_service.py
+++ src/services/testimonial_service.py
@@ -9,65 +9,83 @@ from src.utils.backoff_helper import backoff_handler
 from src.utils.logUtil import log
 
 
-@backoff.on_exception(
-    backoff.expo,  # Exponential backoff strategy
-    OperationalError,  # Type of exception to retry on
-    max_tries=3,  # Maximum retries
-    on_backoff=backoff_handler  # Log or handle retry attempt
-)
-def get_all_testimonials(db_session: Session) -> list[TestimonialResponse]:
-    log.info("Getting all testimonials sorted by rating (desc)")
+class TestimonialService:
 
-    try:
-        testimonials = (
-            db_session.query(TestimonialModel)
-            .order_by(TestimonialModel.rating.desc())
-            .all()
-        )
+    @backoff.on_exception(backoff.expo, OperationalError, max_tries=3, on_backoff=backoff_handler)
+    def get_all_testimonials(self, db_session: Session) -> list[TestimonialResponse]:
+        log.info("Getting all testimonials sorted by rating (desc)")
 
-        if not testimonials:
-            log.info("No testimonials found")
-            return []
+        try:
+            testimonials = (
+                db_session.query(TestimonialModel)
+                .order_by(TestimonialModel.rating.desc())
+                .all()
+            )
 
-        return [TestimonialResponse.from_orm(testimonial) for testimonial in testimonials]
+            if not testimonials:
+                log.info("No testimonials found")
+                return []
 
-    except OperationalError as e:
-        log.error(f"Operational error while fetching testimonials: {e}", exc_info=True)
-        raise HTTPException(status_code=500, detail="Database error occurred")
+            return [TestimonialResponse.from_orm(testimonial) for testimonial in testimonials]
 
-    except Exception as e:
-        log.error(f"Unexpected error while fetching testimonials: {e}", exc_info=True)
-        raise HTTPException(status_code=500, detail="Internal server error")
+        except OperationalError as e:
+            log.error(f"Operational error while fetching testimonials: {e}", exc_info=True)
+            raise HTTPException(status_code=500, detail="Database error occurred")
 
+        except Exception as e:
+            log.error(f"Unexpected error while fetching testimonials: {e}", exc_info=True)
+            raise HTTPException(status_code=500, detail="Internal server error")
 
-@backoff.on_exception(
-    backoff.expo,
-    OperationalError,
-    max_tries=3,
-    on_backoff=backoff_handler
-)
-def create_testimonial(testimonial: TestimonialCreate, db_session: Session) -> TestimonialResponse:
-    log.info("Creating a new testimonial")
+    @backoff.on_exception(backoff.expo, OperationalError, max_tries=3, on_backoff=backoff_handler)
+    def create_testimonial(self, testimonial: TestimonialCreate, db_session: Session) -> TestimonialResponse:
+        log.info("Creating a new testimonial")
 
-    try:
-        new_testimonial = TestimonialModel(
-            first_name=testimonial.first_name,
-            last_name=testimonial.last_name,
-            rating=testimonial.rating,
-            comment=testimonial.comment
-        )
-        db_session.add(new_testimonial)
-        db_session.commit()
-        db_session.refresh(new_testimonial)
+        try:
+            new_testimonial = TestimonialModel(
+                first_name=testimonial.first_name,
+                last_name=testimonial.last_name,
+                rating=testimonial.rating,
+                comment=testimonial.comment
+            )
+            db_session.add(new_testimonial)
+            db_session.commit()
+            db_session.refresh(new_testimonial)
 
-        return TestimonialResponse.from_orm(new_testimonial)
+            return TestimonialResponse.from_orm(new_testimonial)
 
-    except OperationalError as e:
-        db_session.rollback()
-        log.error(f"Database error. Exception: {e}", exc_info=True)
-        raise HTTPException(status_code=500, detail="Database Error")
+        except OperationalError as e:
+            db_session.rollback()
+            log.error(f"Database error. Exception: {e}", exc_info=True)
+            raise HTTPException(status_code=500, detail="Database Error")
 
-    except Exception as e:
-        db_session.rollback()
-        log.error(f"Error creating testimonial. Exception: {e}", exc_info=True)
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+        except Exception as e:
+            db_session.rollback()
+            log.error(f"Error creating testimonial. Exception: {e}", exc_info=True)
+            raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    @backoff.on_exception(backoff.expo, OperationalError, max_tries=3, on_backoff=backoff_handler)
+    def delete_testimonial(self, testimonial_id: str, db_session: Session) -> bool:
+        log.info(f"Deleting testimonial with id: {testimonial_id}")
+
+        try:
+
+            testimonial_db = db_session.query(TestimonialModel).filter(TestimonialModel.id == testimonial_id).one_or_none()
+
+            if not testimonial_db:
+                log.warning(f"Testimonial with id: {testimonial_id} not found")
+                return False
+
+            db_session.delete(testimonial_db)
+            db_session.commit()
+
+            return True
+
+        except OperationalError as e:
+            db_session.rollback()
+            log.error(f"Database error. Exception: {e}", exc_info=True)
+            raise HTTPException(status_code=500, detail="Database Error")
+
+        except Exception as e:
+            db_session.rollback()
+            log.error(f"Error creating testimonial. Exception: {e}", exc_info=True)
+            raise HTTPException(status_code=500, detail="Internal Server Error")
