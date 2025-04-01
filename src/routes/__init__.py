30af63c1b0ea6d947b05d8b98359a106836df10e
Alonso VN <alonsonh94@gmail.com>
2025-03-31 22:39:35 -0600

    Adding api version

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -4,8 +4,9 @@ from src.routes import email_routes, testimonial_routes, user_routes
 
 # Create a main router to include all route modules
 router = APIRouter()
+api_version = "/api/v1"
 
 # Include individual routers
-router.include_router(email_routes.router, prefix="/email", tags=["email"])
-router.include_router(testimonial_routes.router, prefix="/testimonial", tags=["testimonial"])
-router.include_router(user_routes.router, prefix="/user", tags=["user"])
+router.include_router(email_routes.router, prefix=f"{api_version}/email", tags=["email"])
+router.include_router(testimonial_routes.router, prefix=f"{api_version}/testimonial", tags=["testimonial"])
+router.include_router(user_routes.router, prefix=f"{api_version}/user", tags=["user"])
