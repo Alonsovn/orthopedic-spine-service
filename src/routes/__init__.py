8fad8add2223ce01e04e7d6bb752bd2776a7a3a3
Alonso VN <alonsonh94@gmail.com>
2025-05-08 17:56:27 -0600

    Adding database test endpoint

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from src.routes import email_routes, testimonial_routes, user_routes
+from src.routes import email_routes, testimonial_routes, user_routes, database_routes
 
 # Create a main router to include all route modules
 router = APIRouter()
@@ -10,3 +10,4 @@ api_version = "/api/v1"
 router.include_router(email_routes.router, prefix=f"{api_version}/email", tags=["email"])
 router.include_router(testimonial_routes.router, prefix=f"{api_version}/testimonial", tags=["testimonial"])
 router.include_router(user_routes.router, prefix=f"{api_version}/user", tags=["user"])
+router.include_router(database_routes.router, prefix=f"{api_version}/database", tags=["database"])
